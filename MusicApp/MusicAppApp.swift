a0441a6f6a8f0e5bc1d5ac842b35a7afee968ce1
Anna Taylor <annatnyc@gmail.com>
2021-01-29 16:09:05 -0500

    Initial Commit

---
+++ MusicApp/MusicAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicAppApp.swift
+//  MusicApp
+//
+//  Created by Anna Taylor on 1/29/21.
+//
+
+import SwiftUI
+
+@main
+struct MusicAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
